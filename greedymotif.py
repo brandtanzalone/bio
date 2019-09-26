963ca7844a871039d1491af16e8ee8826017b6a7
brandtanzalone <brandtanzalone@gmail.com>
2019-09-26 12:58:01 -0400

    mogos

--- greedymotif.py
+++ greedymotif.py
@@ -19,15 +19,54 @@ mot = text[1:]
 for x in mot:
     x.strip('\n')
 
-chmap = {'A':0,'C':1,'G':2,'T':3}
-pmat = torch.zeros([4,k])
-print(pmat)
-for x in range(len(mot)):
-    #compute matrix of everything else
+def scorekmer(kmer,profile,charm):
+    prob = 0
+    for i in range(len(kmer)):
+        prob = prob*profile[charm[kmer[i]],i]
+        print(prob)
+    ent = prob*math.log(prob,2)
+    return ent
+
+def mlk(dna,k,prof,cmap):
+    bestkmer = ''
+    smax = 0
+    for x in range(len(dna)-k):
+        score = scorekmer(dna[x:x+k],prof,cmap)
+        if score > smax:
+            smax = score
+            bestkmer = dna[x:x+k]
+    return bestkmer
+
+def buildprofile(motifs,k,t,cmap):
     pmat = torch.zeros([4,k])
-    e = mot[:x] + mot[x+1:]
-    for x in e:
+    for x in motifs:
         for y in range(k):
-            pmat[chmap[x[y]],y]+=1
-    print(pmat)
-    for y in range(len(x) - k):
+            pmat[cmap[x[y]],y]+=1
+    for i,x in enumerate(pmat):
+        for j,y in enumerate(x):
+            pmat[i,j] = pmat[i,j]/t
+    return pmat
+
+def pscore(profile):
+    print(profile)
+    cmax = 0
+    p = 0
+    # for x in range(k):
+    #     for y in range(4):
+
+
+
+chmap = {'A':0,'C':1,'G':2,'T':3}
+motifs = []
+for x in range(len(mot[0]) - k):
+    m = [mot[0][x:x+k]]
+    seed = buildprofile(m,k,t,chmap)
+    for y in range(1,t):
+        m.append([mlk(mot[y],k,seed,chmap)])
+        seed = buildprofile(m,k,t,chmap)
+    motifs.append((seed,m))
+
+mscore = 0
+for x in motifs:
+    print(pscore(x[0]))
+    
